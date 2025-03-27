9de982c4c8575151c02e4d6465bd50fdc7755f46
KhareAnupam <a.khare9149@gmail.com>
2025-03-28 01:56:25 +0530

    Initial Commit: Project setup and core files added

---
+++ models/listing.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const { required } = require("nodemon/lib/config");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title : {
+        type: String,
+        required : true
+    },
+    description : String,
+    image :{
+        type: String,
+        default:"https://images.unsplash.com/photo-1742567009397-c64925e0c3ba?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        set: (v) => v === "" ? "https://images.unsplash.com/photo-1742567009397-c64925e0c3ba?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        : v
+    },
+    price : Number,
+    location: String,
+    country : String
+});
+
+module.exports = mongoose.model("Listing", listingSchema);
