add91d6490ea79b19d356834c57fcee55d2b7e37
KhareAnupam <a.khare9149@gmail.com>
2025-04-02 20:17:32 +0530

    Review Form Added (Front-End)

---
+++ models/review.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const { required } = require("nodemon/lib/config");
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+    comment: String,
+    rating: {
+        type: number,
+        min: 1,
+        max: 5
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+module.exports = mongoose.model("Review", reviewSchema);
