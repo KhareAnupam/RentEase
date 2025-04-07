a18c0439a21087b9b52b2af5523dfe5b02224b45
KhareAnupam <a.khare9149@gmail.com>
2025-04-07 17:44:49 +0530

    Handle Listings deletions (respective reviews will also get deleted)

--- models/listing.js
+++ models/listing.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const { required } = require("nodemon/lib/config");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
 
 const listingSchema = new Schema({
     title : {
@@ -23,6 +24,12 @@ const listingSchema = new Schema({
             ref: "Review",
         }
     ]
+});
+
+listingSchema.post("findOneAndDelete", async (listing)=>{
+    if(listing){
+        await Review.deleteMany({_id : {$in: listing.reviews}});
+    }
 });
 
 module.exports = mongoose.model("Listing", listingSchema);
