ea001922e0f1f2d122bb110f400422f728f83fc0
KhareAnupam <a.khare9149@gmail.com>
2025-04-10 21:46:20 +0530

    Express-Session added

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError =  require("./utils/ExpressError.js");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
@@ -36,12 +38,22 @@ app.use(express.static(path.join(__dirname,"/public")));
 //     next();
 // });
 
+const sessionOptions = {
+    secret: "mysupersecretcode",
+    resave: false,
+    saveUninitialized: true,
+    cookie:{
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+    },
+}
+
+app.use(session(sessionOptions));
+
 app.get("/", (req,res) =>{
     res.send("hi, I am root")
 })
-
-
-
 
 
 app.use("/listings", listings);
