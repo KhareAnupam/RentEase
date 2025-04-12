29424d0ff865c6d51e2f208657b0a7ec33ac9bba
KhareAnupam <a.khare9149@gmail.com>
2025-04-12 20:19:30 +0530

    Fix typo in passport-local-mongoose import

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
@@ -14,6 +13,8 @@ const User = require("./models/user.js");
 
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 app.engine('ejs', ejsMate);
 
@@ -61,7 +62,7 @@ app.use(flash());
 
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(new LocalStrategy(User.authentication()));
+passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
@@ -70,7 +71,17 @@ app.use((req,res,next) =>{
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
-})
+});
+
+app.get("/demo", async(req,res)=>{
+    let fakeUser = new User({
+        email: "student@gmail.com",
+        username: "anupam-khare"
+    })
+    let registerUser = await User.register(fakeUser, "helloworld");
+    res.send(registerUser);
+});
+
 
 app.use("/listings", listings);
 app.use("/listings/:id/reviews", reviews )
