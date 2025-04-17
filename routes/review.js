7283d61dc44024686697782742eec1106213c657
KhareAnupam <a.khare9149@gmail.com>
2025-04-17 11:45:07 +0530

    Owner reference added

--- routes/review.js
+++ routes/review.js
@@ -3,19 +3,9 @@ const router = express.Router({mergeParams:true});
 const wrapAsync =  require("../utils/wrapAsync.js");
 const ExpressError =  require("../utils/ExpressError.js");
 const Review = require("../models/review");
-const {reviewSchema} = require("../schema.js")
 const Listing = require("../models/listing");   //because we also adds and delete reviews from Listings
-const { isLoggedIn } = require("../middleware.js");
+const {validateReview } = require("../middleware.js");
 
-const validateReview = (req,res,next)=>{
-    let {error} = reviewSchema.validate(req.body);
-    if(error){
-        let errMsg = error.details.map((el)=> el.message).join(",");
-        throw new ExpressError(400,errMsg);
-    }else{
-        next();
-    }
-}
 
 //Reviews
 //
