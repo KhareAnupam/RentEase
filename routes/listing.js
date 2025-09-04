5f9ee4b2713fe8a0b4ff8d58fb6cf38b1290ee77
KhareAnupam <a.khare9149@gmail.com>
2025-09-04 11:34:05 +0530

    router.route update (compact code and better readability

--- routes/listing.js
+++ routes/listing.js
@@ -8,27 +8,38 @@ const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 
 const listingController = require("../controllers/listing.js");
 
-//Index route
-router.get("/",wrapAsync(listingController.index));
+
+router.route("/")     //  (1)
+        .get(wrapAsync(listingController.index))  //Index route
+        .post(isLoggedIn , validateListing ,wrapAsync(listingController.createListing)); // Create route
+
 
 //New Route
+router.get("/new", isLoggedIn ,listingController.renderNewForm);        
+    
+router.route("/:id")  //  (2)
+        .get( wrapAsync(listingController.showListings)) //Show route
+        .put(isLoggedIn,isOwner ,validateListing , wrapAsync(listingController.updateListing)) // update route
+        .delete(isLoggedIn, isOwner ,wrapAsync(listingController.destroyListing));  // Delete route
 
-router.get("/new", isLoggedIn ,listingController.renderNewForm)
-
-//Show route
-router.get("/:id", wrapAsync(listingController.showListings));
-
-//Create route
-
-router.post("/",isLoggedIn , validateListing ,wrapAsync(listingController.createListing));
 
 //Edit route
 router.get("/:id/edit",isLoggedIn,isOwner , wrapAsync(listingController.renderEditForm));
 
-//Update route
-router.put("/:id",isLoggedIn,isOwner ,validateListing , wrapAsync(listingController.updateListing)); 
+//(1) 
+//Index route
+// router.get("/",wrapAsync(listingController.index));
+//Create route
+// router.post("/",isLoggedIn , validateListing ,wrapAsync(listingController.createListing));
 
+//(2)
+//Show route
+// router.get("/:id", wrapAsync(listingController.showListings));
+//Update route
+// router.put("/:id",isLoggedIn,isOwner ,validateListing , wrapAsync(listingController.updateListing)); 
 //Delete route
-router.delete("/:id",isLoggedIn, isOwner ,wrapAsync(listingController.destroyListing));
+// router.delete("/:id",isLoggedIn, isOwner ,wrapAsync(listingController.destroyListing));
+
+
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -7,20 +7,27 @@ const e = require("connect-flash");
 const passport = require("passport");
 
 const userController = require("../controllers/users.js");
-//singup
 
-router.get("/signup", userController.renderSignupForm );
+router.route("/signup")
+        .get(userController.renderSignupForm)   // signup form render
+        .post(wrapAsync(userController.signup));  //sign up
 
-router.post("/signup", wrapAsync(userController.signup));
-
-//login
-
-router.get("/login", userController.renderLoginForm);
-
-router.post("/login",
-    passport.authenticate("local", {failureRedirect: '/login', failureFlash: true}),
-    userController.login);
+router.route("/login")
+        .get(userController.renderLoginForm)   // login from render
+        .post(passport.authenticate("local",   // login
+            {failureRedirect: '/login', failureFlash: true}),userController.login);
 
 router.get("/logout", userController.logout);
+
+
+//signup
+// router.get("/signup", userController.renderSignupForm );
+// router.post("/signup", wrapAsync(userController.signup));
+
+//login
+// router.get("/login", userController.renderLoginForm);
+// router.post("/login",
+//     passport.authenticate("local", {failureRedirect: '/login', failureFlash: true}),
+//     userController.login);
 
 module.exports = router;
